da6814b651a5243511b56cf5597ba106cac55bda
Helerson Junior <helersonjunior@gmail.com>
2024-03-14 20:20:20 -0400

    nova

--- assets/js/tema.js
+++ assets/js/tema.js
@@ -1,26 +1,48 @@
 
 let padrao = document.querySelectorAll('.padrao')
 let tema = document.querySelector('.temas')
+let as = document.querySelectorAll('.a')
+
 
 
 //---- mudar de tema e setar na local storage
 tema.onclick = () => {
-
-   
-
+      
    padrao.forEach( p => {
         
     if(p.classList.contains('dia')){
+
         p.classList.remove('dia')
         p.classList.add('noite')
+
+
         localStorage.setItem('tema', 'noite')
     }else{
+
+        
         p.classList.remove('noite')
         p.classList.add('dia')
+
         localStorage.setItem('tema', 'dia')
     }
 
+
+        as.forEach(a => {
+
+            if(p.classList.contains('noite')){
+                a.classList.remove('ancora-dia')
+                a.classList.add('ancora-noite')
+            }else{
+                a.classList.remove('ancora-noite')
+                a.classList.add('ancora-dia')
+            }
+            
+        })
+       
+
    });
+
+   
    
 }
 
@@ -31,5 +53,18 @@ window.onload = function () {
     padrao.forEach(p => {
         p.classList.add(localStorage.getItem('tema'))
     })
+
+    as.forEach(a => {
+        if(localStorage.getItem('tema') == 'noite'){
+            a.classList.remove('ancora-dia')
+            a.classList.add('ancora-noite')
+        }else{
+            a.classList.remove('ancora-noite')
+            a.classList.add('ancora-dia')
+        }
+        
+    })
+   
+    
     
 }
